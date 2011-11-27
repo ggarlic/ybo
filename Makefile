3303da5061ac90e0ab49f97909ec633ff5a93ef1
ggarlic yang <yangbo.ggarlic@gmail.com>
2011-11-27 11:11:49 +0800

    Signed-off-by: ggarlic yang <yangbo.ggarlic@gmail.com>

--- Makefile
+++ Makefile
@@ -3,8 +3,15 @@ LIBS=`pkg-config --libs gtk+-2.0 gstreamer-0.10`
 
 all: ybo
 
-ybo: main.o
-	gcc -o ybo main.o $(LIBS)
+ybo: main.o playback.o
+	gcc -o player main.o playback.o $(LIBS)
 
-%.o: %.c
-	gcc -g -c $< $(CFLAGS)
+playback.o: playback.c playback.h
+	gcc -g -c playback.c $(CFLAGS)
+
+main.o: main.c playback.h
+	gcc -g -c main.c $(CFLAGS)
+
+clean:
+	rm *.o player
+
--- main.h
+++ main.h
@@ -1,23 +1,23 @@
-/*
- * =====================================================================================
- *
- *       Filename:  main.h
- *
- *    Description:  main header file
- *
- *        Version:  1.0
- *        Created:  10/24/2011 10:08:19 PM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  yang bo (ggarlic), yangbo.ggarlic@gmail.com
- *        Company:  
- *
- * =====================================================================================
- */
 #ifndef MAIN_H
 #define MAIN_H
 
 #include <gtk/gtk.h>
 
+
+typedef enum {
+  STATE_STOP,
+  STATE_PLAY,
+  STATE_PAUSE
+} PlayerState;
+
+
+void gui_status_update(PlayerState state);
+void gui_update_time(const gchar *time,
+                     const gint64 position,
+		     const gint64 length);
+void gui_update_metadata(const gchar *title,
+                         const gchar *artist);
+
+
 #endif
+
